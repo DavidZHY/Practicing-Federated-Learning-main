bcdfa82b86f2abd0379221d35c543393e0e0c1a2
RattleSnack <david.z2016@outlook.com>
2023-11-29 16:09:51 +0800

    main

--- chapter03_Python_image_classification/models.py
+++ chapter03_Python_image_classification/models.py
@@ -1,14 +1,14 @@
 
-import torch 
+import torch
 from torchvision import models
 
 def get_model(name="vgg16", pretrained=True):
 	if name == "resnet18":
 		model = models.resnet18(pretrained=pretrained)
 	elif name == "resnet50":
-		model = models.resnet50(pretrained=pretrained)	
+		model = models.resnet50(pretrained=pretrained)
 	elif name == "densenet121":
-		model = models.densenet121(pretrained=pretrained)		
+		model = models.densenet121(pretrained=pretrained)
 	elif name == "alexnet":
 		model = models.alexnet(pretrained=pretrained)
 	elif name == "vgg16":
@@ -17,10 +17,10 @@ def get_model(name="vgg16", pretrained=True):
 		model = models.vgg19(pretrained=pretrained)
 	elif name == "inception_v3":
 		model = models.inception_v3(pretrained=pretrained)
-	elif name == "googlenet":		
+	elif name == "googlenet":
 		model = models.googlenet(pretrained=pretrained)
-		
+
 	if torch.cuda.is_available():
-		return model.cuda()
+		return model_selection.cuda()
 	else:
-		return model 
+		return model
